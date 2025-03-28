ff3f7462e5fcc1fae752597a7027a68997b2483f
CODE-BLANK01 <105882625+CODE-BLANK01@users.noreply.github.com>
2025-03-28 01:05:25 -0700

    questions model implemented

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Question(models.Model):
+    ACCESS_CHOICES = [
+        ('public', 'Public'),
+        ('course', 'Course Only'),
+        ('private', 'Private'),
+    ]
+
+    title = models.CharField(max_length=255)
+    topic = models.CharField(max_length=100)
+    difficulty_level = models.CharField(max_length=50)
+    description = models.TextField()
+    access_type = models.CharField(max_length=20, choices=ACCESS_CHOICES)
+    created_date = models.DateField(auto_now_add=True)
+    creator = models.ForeignKey('users.User', on_delete=models.CASCADE)
