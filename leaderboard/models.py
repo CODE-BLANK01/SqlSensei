1d5b3060190660ff9cdb6d9fa6f866dcad7ce2ce
CODE-BLANK01 <105882625+CODE-BLANK01@users.noreply.github.com>
2025-03-28 01:09:32 -0700

    leaderboard model implemented

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Leaderboard(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    week = models.CharField(max_length=50)
+    problems_solved = models.IntegerField()
+    score = models.FloatField()
+
+    def __str__(self):
+        return f"{self.student} - Week {self.week} - Score {self.score}"
+
+
+class ParticipationStreak(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    streak_count = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student} - Streak: {self.streak_count}"
